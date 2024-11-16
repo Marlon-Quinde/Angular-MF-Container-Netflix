cd7162b49dd9337bec0dddf2d6a7135f9f62bf4b
mquinde <mquinde@viamatica.me>
2024-11-15 21:34:00 -0500

    feat(ToolBar/SideNav): :sparkles: Implementacion del Toolbar y el SideNav

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,12 +5,14 @@ import { HomeRoutingModule } from './home-routing.module';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { SharedModule } from '../../shared/shared.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 
 @NgModule({
   declarations: [
     LayoutComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    SidenavComponent
   ],
   imports: [
     CommonModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,6 +9,9 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 
 @NgModule({
@@ -22,7 +25,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSelectModule,
+    MatSidenavModule
   ]
 })
 export class SharedModule { }
